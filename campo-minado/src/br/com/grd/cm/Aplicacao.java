2478739527420ad4d653d77e3b3e197829e38af0
Guilherme Ribeiro <guilherme_r_demetrio@hotmail.com>
2026-04-17 16:20:36 -0300

    Update Aplicacao.java

--- campo-minado/src/br/com/grd/cm/Aplicacao.java
+++ campo-minado/src/br/com/grd/cm/Aplicacao.java
@@ -8,7 +8,7 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		new TabuleiroConsole(tabuleiro);
+		//new TabuleiroConsole(tabuleiro);
 		
 		
 		
