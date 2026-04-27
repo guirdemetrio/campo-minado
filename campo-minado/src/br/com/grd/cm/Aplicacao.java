106808e9f2c69bc0e6e44d55d2408a2252d89cd5
Guilherme Ribeiro <guilherme_r_demetrio@hotmail.com>
2026-04-27 17:33:23 -0300

    Update Aplicacao.java

--- campo-minado/src/br/com/grd/cm/Aplicacao.java
+++ campo-minado/src/br/com/grd/cm/Aplicacao.java
@@ -6,7 +6,7 @@ import br.com.grd.cm.visao.TabuleiroConsole;
 public class Aplicacao {
 	
 	public static void main(String[] args) {
-		
+		//roda a aplicação, para isso tem que tirar o comentário do TabuleiroConsole!!!
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		//new TabuleiroConsole(tabuleiro);
 		
